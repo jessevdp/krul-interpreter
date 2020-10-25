c2719235479d42b8cb48239454e265fabe4327c1
Jesse van der Pluijm <jessevdp@hey.com>
2020-10-25 16:28:33 +0100

    Create CallStack implementation VectorCallStack

---
+++ src/interpreter/VectorCallStack.cpp
@@ -0,0 +1,16 @@
+#include "VectorCallStack.h"
+
+namespace krul::interpreter {
+
+  void VectorCallStack::push(line_t line) {
+    _vector.push_back(line);
+  }
+
+  line_t VectorCallStack::pop() {
+    if (_vector.empty()) throw std::out_of_range("VectorCallStack is empty");
+    line_t item = _vector.back();
+    _vector.pop_back();
+    return item;
+  }
+
+}
